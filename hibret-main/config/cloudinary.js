4126f1b0684d05503b7dfb49319ac196c31d861f
Tesnim19 <tesnimabdi67@gmail.com>
2024-04-25 01:28:47 +0300

    file uploading configurations

---
+++ hibret-main/config/cloudinary.js
@@ -0,0 +1,9 @@
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: 'dfcnzxtbo', 
+  api_key: '589146296757742', 
+  api_secret: 'i1ESogdAgebw0DF3Z2dHbZtLXRs' 
+});
+
+export default cloudinary;
