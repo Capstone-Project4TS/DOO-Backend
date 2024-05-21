4fdc9ba5954d7f634e82cf415553f3a542da0ffe
sitra061212 <sitra.f.khairo511@gmail.com>
2024-05-21 17:27:18 +0300

    workflow template

--- hibret-main/controllers/workflowTemplateController.js
+++ hibret-main/controllers/workflowTemplateController.js
@@ -1,4 +1,4 @@
-import Workflow from '../models/workflow.model.js';
+import WorkflowTemplate from '../models/workflowTemplate.model.js';
 import DocumentTemplate from '../models/documentTemplate.model.js'
 import mongoose from 'mongoose';
 
@@ -97,6 +97,27 @@ export async function getAllWorkflowTemplates(req, res) {
   }
 }
 
+// Get all workflow templates
+export async function getAllRequiredDocumentTemplates(req, res) {
+
+  try {
+    const template = await WorkflowTemplate.findById(req.params.id).populate('requiredDocumentTemplates');
+    if (!template) {
+      return res.status(404).json({ message: 'Workflow template not found' });
+    }
+
+    console.log('Template:', template);
+
+    const documents = template.requiredDocumentTemplates;
+    const additional = template.additionalDoc;
+
+
+    // Send document contents array as response
+    res.status(200).json({ documents, additional });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}
 
 // Get a single workflow template by ID
 export async function getWorkflowTemplateById(req, res) {
