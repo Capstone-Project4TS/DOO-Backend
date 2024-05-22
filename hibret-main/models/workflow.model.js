63212a72da78fa9e6ed3f8a7889059937530fca5
Tesnim19 <tesnimabdi67@gmail.com>
2024-05-22 23:04:36 +0300

    sanitize get workflows for owner

--- hibret-main/models/workflow.model.js
+++ hibret-main/models/workflow.model.js
@@ -2,6 +2,10 @@ import { Schema, model } from 'mongoose';
 
   
   const WorkflowSchema = new Schema({
+    name: {
+        type: String,
+        // required: true
+    },
   
     workflowTemplate: {
         type: Schema.Types.ObjectId,
