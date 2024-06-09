ba47e5024347d90166cde70922b669648cceedc1
sitra061212 <sitra.f.khairo511@gmail.com>
2024-06-09 14:29:16 +0300

    email

--- hibret-main/services/emailService.js
+++ hibret-main/services/emailService.js
@@ -18,7 +18,7 @@ export const sendInvitation = async (email, username, password) => {
     subject: "Log into your Account by using this password",
     html: `<p>Hello ${username},</p>
            <p>Your default password is: <strong>${password}</strong></p>
-           <p>Please verify your email by clicking this link: <a href="http://${process.env.CLIENT_URL}/account/confirm">http://${process.env.CLIENT_URL}/account/confirm</a></p>`,
+           <p>Please verify your email by clicking this link: <a href="${process.env.CLIENT_URL}/login">http://${process.env.CLIENT_URL}/account/confirm</a></p>`,
   };
   return mailOptions;
 };
@@ -30,7 +30,7 @@ export const sendPasswordResetCode = async (email, username, otp) => {
     subject: "Here is the reset code verify it!",
     html: `<p>Hello ${username},</p>
          <p>Your reset code is: <strong>${otp}</strong></p>
-         <p>Please verify your code by clicking this link: <a href="http://${process.env.CLIENT_URL}/account/confirm">http://${process.env.CLIENT_URL}/account/confirm</a></p>`,
+         <p>Please verify your code by clicking this link: <a href="${process.env.CLIENT_URL}/">http://${process.env.CLIENT_URL}/account/confirm</a></p>`,
   };
   return mailOptions;
 };
